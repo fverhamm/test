8e032f4fedd7d8b0a64ace30a752a075edbedbef
guest <guest@howest.be>
2017-05-14 08:45:42 +0200

    change in master

---
+++ oef_modorg.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM biodb.modorg
+LIMIT 0, 1000
+
+-- Date: 2017-05-13 19:14
+*/
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (1,'Bacteria','Escherichia','coli',1,4.639,'1997-09-05 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (2,'Fungi','Saccharomyces','cerevisiae',16,0.672,'1996-10-25 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (3,'Roundworms','Caenorhabditis','elegans',6,100,'1998-12-16 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (4,'Insects','Drosophila','melanogester',4,180,'2000-03-25 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (5,'Insects','Apis','melifera',16,218,'2003-12-19 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (6,'Mammals','Mus','musculus',21,2651,'2010-09-23 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (7,'Mammals','Rattus','norvegicus',21,6597,'2004-04-02 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (8,'Mammals','Gorilla','gorilla',24,3076,'2008-11-03 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (9,'Mammals','Homo','sapiens',23,3038,'2007-05-22 00:00:00');
+INSERT INTO `modorg_exp` (`id`,`class`,`genus`,`species`,`nchr`,`gsize`,`draft`) VALUES (10,'Aliens','Alienus','area51us',NULL,NULL,NULL);
